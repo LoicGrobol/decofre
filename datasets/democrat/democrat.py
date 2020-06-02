4b399946f2f85574588da6cbd380421d39cc1cc4
Loïc Grobol <loic.grobol@gmail.com>
2020-06-02 21:55:42 +0200

    fix duplicated spans

--- datasets/democrat/democrat.py
+++ datasets/democrat/democrat.py
@@ -44,7 +44,7 @@ def generate_cluster_file(annotation_path: pathlib.Path, clusters_path: pathlib.
 def generate_dataset(source_dir: pathlib.Path, target_dir: pathlib.Path):
     spans_dir = target_dir / "mentions"
     antecedents_dir = target_dir / "antecedents"
-    text_lst = list((source_dir / "xml").glob("*.xml"))
+    text_lst = sorted((source_dir / "xml").glob("*.xml"))
     for text in tqdm.tqdm(text_lst, unit="documents", desc="Building datafiles"):
         annotation = source_dir / "urs" / f"{text.stem}-urs.xml"
         generate_spans_and_antecedents(text, annotation, spans_dir, antecedents_dir)
@@ -53,7 +53,7 @@ def generate_dataset(source_dir: pathlib.Path, target_dir: pathlib.Path):
 def generate_clusters(source_dir: pathlib.Path, target_dir: pathlib.Path):
     clusters_dir = target_dir / "clusters"
     clusters_dir.mkdir(exist_ok=True, parents=True)
-    text_lst = list((source_dir / "xml").glob("*.xml"))
+    text_lst = sorted((source_dir / "xml").glob("*.xml"))
     for text in tqdm.tqdm(text_lst, unit="documents", desc="Building cluster files"):
         annotation = source_dir / "urs" / f"{text.stem}-urs.xml"
         clusters_file = clusters_dir / f"{text.stem}.json"
