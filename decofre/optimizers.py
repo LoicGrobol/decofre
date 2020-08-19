e2a3f886d6c528863e691155c78d7866e064c546
Loïc Grobol <loic.grobol@gmail.com>
2020-08-19 10:34:47 +0200

    fix deprecation warning for addcmul_

--- decofre/optimizers.py
+++ decofre/optimizers.py
@@ -104,7 +104,7 @@ class DenseSparseAdamW(Optimizer):
 
                     # Decay the first and second moment running average coefficient
                     exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-                    exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                    exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
 
                     denom = exp_avg_sq.sqrt().add_(group["eps"])
 
