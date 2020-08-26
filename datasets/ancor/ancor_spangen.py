1c03c3b6ffe143941fa85d897d8ab026b77b6fbb
Loïc Grobol <loic.grobol@gmail.com>
2020-08-26 11:46:15 +0200

    fix morph in ancor spangen

--- datasets/ancor/ancor_spangen.py
+++ datasets/ancor/ancor_spangen.py
@@ -448,8 +448,10 @@ MentionFeaturesDict = TypedDict(
 
 def morph_from_tag(tag: str) -> ty.List[str]:
     """Extract morphosyntax features from spaCy tag str."""
-    pos, rest = tag.split("__", maxsplit=1)
-    return rest.split("|")
+    pos, *rest = tag.split("__", maxsplit=1)
+    if not rest:
+        return []
+    return rest[0].split("|")
 
 
 def spans_from_doc(
